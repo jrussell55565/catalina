212695cb4db7cf463460b07b35ebce8f33f7c059
Matt <mjtice@gmail.com>
2015-11-07 21:08:18 -0700

    fixed links

--- dist/menus_sidebars_elements/topmenuanimation.php
+++ dist/menus_sidebars_elements/topmenuanimation.php
@@ -5,11 +5,11 @@
           <div class="row">
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="/pages/dispatch/orders.php">
+<a href="<?php echo HTTP;?>/pages/dispatch/orders.php">
                  <span class="info-box-icon bg-aqua"><i class="fa fa-cog fa-spin"></i></span>
                 </a>               
                 <div class="info-box-content">
-<span class="info-box-text"><a href="/pages/dispatch/orders.php"> My Load Board</a></span><span class="info-box-number">  Todays  PU:  <?php echo "$pu_today_count";?><br>
+<span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/orders.php"> My Load Board</a></span><span class="info-box-number">  Todays  PU:  <?php echo "$pu_today_count";?><br>
                 Todays DEL:   <?php echo "$del_today_count";?></span></div>
                 <!-- /.info-box-content -->
               </div><!-- /.info-box -->
@@ -19,7 +19,7 @@
 
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="/pages/dispatch/vir.php" class="button animated rubberBand">
+<a href="<?php echo HTTP;?>/pages/dispatch/vir.php" class="button animated rubberBand">
 <style>
  a.button {
 	 -webkit-animation-duration: 5s;
@@ -30,7 +30,7 @@
                 <span class="info-box-icon bg-red"><i class="fa fa-wrench faa-wrench animated"></i></span>
 </a>               
                 <div class="info-box-content">
-                  <p><span class="info-box-text"><a href="/pages/dispatch/vir.php">My VIRs</a></span>
+                  <p><span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/vir.php">My VIRs</a></span>
                     <span class="info-box-number">                   Todays  VIRS:  <?php echo "$pu_today_count";?><br>
 Last 8 days VIRS: <?php echo "$del_today_count";?></span></p>
 </div>
@@ -43,7 +43,7 @@ Last 8 days VIRS: <?php echo "$del_today_count";?></span></p>
 
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="pages/tables/fuel.php" class="button2 animated zoomIn">
+<a href="<?php echo HTTP;?>/pages/tables/fuel.php" class="button2 animated zoomIn">
 <style>
  a.button2 {
 	 -webkit-animation-duration: 6s;
@@ -54,7 +54,7 @@ Last 8 days VIRS: <?php echo "$del_today_count";?></span></p>
                 <span class="info-box-icon bg-green"><i class="fa fa-tachometer"></i></span>
                 </a>
                 <div class="info-box-content">
-                <span class="info-box-text"><a href="pages/dispatch/fuel.php">Company Stats</a></span><span class="info-box-number">Todays  Fuel: <?php echo "$pu_today_count";?><br>
+                <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/fuel.php">Company Stats</a></span><span class="info-box-number">Todays  Fuel: <?php echo "$pu_today_count";?><br>
 Last 8 days Fuel: <?php echo "$del_today_count";?></span></div>
                 <!-- /.info-box-content -->
               </div><!-- /.info-box -->
@@ -64,7 +64,7 @@ Last 8 days Fuel: <?php echo "$del_today_count";?></span></div>
 
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="pages/tables/ifta.php" class="button3 animated jello">
+<a href="<?php echo HTTP;?>/pages/tables/ifta.php" class="button3 animated jello">
 <style>
  a.button3 {
 	 -webkit-animation-duration: 3s;
@@ -75,7 +75,7 @@ Last 8 days Fuel: <?php echo "$del_today_count";?></span></div>
                <span class="info-box-icon bg-yellow"><i class="fa fa-newspaper-o"></i></span>
 </a>
                 <div class="info-box-content">
-                <span class="info-box-text"><a href="pages/dispatch/ifta.php"> My IFTA/Fuel Reports</a></span><span class="info-box-number">Todays  IFTA: <?php echo "$pu_today_count";?><br>
+                <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/ifta.php"> My IFTA/Fuel Reports</a></span><span class="info-box-number">Todays  IFTA: <?php echo "$pu_today_count";?><br>
 Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-content -->
               </div><!-- /.info-box -->
             </div><!-- /.col -->
@@ -86,7 +86,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
             
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="pages/tables/ifta.php" class="button3 animated pulse">
+<a href="<?php echo HTTP;?>/pages/dispatch/admin/csa.php" class="button3 animated pulse">
 <style>
  a.button3 {
 	 -webkit-animation-duration: 3s;
@@ -98,7 +98,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
 </a>
 
                 <div class="info-box-content">
-                  <span class="info-box-text"><a href="pages/dispatch/dotsafety.php"> My DOT Safety Report</a></span>
+                  <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/admin/csa.php"> My DOT Safety Report</a></span>
                 <span class="info-box-number">                  View Your Report</span></div>
                 <!-- /.info-box-content -->
               </div><!-- /.info-box -->
@@ -107,7 +107,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
 
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="/pages/dispatch/location.php" class="button3 animated pulse">
+<a href="<?php echo HTTP;?>/pages/dispatch/location.php" class="button3 animated pulse">
 <style>
  a.button3 {
 	 -webkit-animation-duration: 3s;
@@ -118,7 +118,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
                <span class="info-box-icon bg-purple"><i class="fa fa-map-marker"></i></span>
                </a>
                 <div class="info-box-content">
-                  <span class="info-box-text"><a href="/pages/dispatch/location.php">Locate Users</a></span><span class="info-box-number"> See where other drivers are</span></div>
+                  <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/location.php">Locate Users</a></span><span class="info-box-number"> See where other drivers are</span></div>
                 <!-- /.info-box-content -->
               </div><!-- /.info-box -->
             </div><!-- /.col -->
@@ -126,7 +126,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
 
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="/pages/dispatch/userstats.php" class="button3 animated tada">
+<a href="<?php echo HTTP;?>/pages/dispatch/userstats.php" class="button3 animated tada">
 <style>
  a.button3 {
 	 -webkit-animation-duration: 3s;
@@ -137,7 +137,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
                <span class="info-box-icon bg-#607D8B"><i class="fa fa-check-square-o"></i></span>
                </a>
                 <div class="info-box-content">
-                  <span class="info-box-text"><a href="/pages/dispatch/userstats.php">  My Stats</a></span><span class="info-box-number">                   View your  Stats</span></div>
+                  <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/userstats.php">  My Stats</a></span><span class="info-box-number">                   View your  Stats</span></div>
                 <!-- /.info-box-content -->
               </div><!-- /.info-box -->
             </div><!-- /.col -->            
@@ -150,7 +150,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
 
 <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="info-box">
-<a href="/pages/dispatch/userprofile.php" class="button4 animated pulse">
+<a href="<?php echo HTTP;?>/pages/dispatch/userprofile.php" class="button4 animated pulse">
 <style>
  a.button4 {
 	 -webkit-animation-duration: 3s;
@@ -161,7 +161,7 @@ Last 8 days IFTA: <?php echo "$del_today_count";?></span></div><!-- /.info-box-c
                <span class="info-box-icon bg-brown"><i class="fa fa-users"></i></span>
                </a>
                 <div class="info-box-content">
-                  <span class="info-box-text"><a href="/pages/dispatch/userprofile.php"> My Profile &amp; Messages</a></span>
+                  <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/userprofile.php"> My Profile &amp; Messages</a></span>
                   <span class="info-box-number">Contact Other Users</span><span class="info-box-number"><br>
                 </span></div>
                 <!-- /.info-box-content -->
