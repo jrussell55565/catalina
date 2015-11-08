b100b67e6a2931438c97ff06c263e7baa43a79e8
Matt <mjtice@gmail.com>
2015-11-08 16:14:45 -0700

    import all csv files

--- bin/getDOT.pl
+++ bin/getDOT.pl
@@ -4,16 +4,17 @@
 
 use DBI;
 use Getopt::Long;
+use File::Find;
 
 # Command line variables
 our ($env, $wwwroot, $ftpusername, $ftppassword, $ftphost, $dbusername, $dbpassword, $dbname);
-GetOptions ("file=s" => \$filePath,
+GetOptions ("file=s" => \@filePath,
             "dbUsername=s" => \$dbusername,
             "dbPassword=s" => \$dbpassword,
             "dbName=s" => \$dbname,
            );
 
-if (  (!$filePath) ||
+if (  (!@filePath) ||
       (!$dbusername) ||
       (!$dbpassword) ||
       (!$dbname) )
@@ -22,67 +23,57 @@ if (  (!$filePath) ||
         exit 1;
     }
 
-if (! -f "$filePath")
-{
-    print "$filePath does not exist\n";
-    exit 1;
-}
-
 our $debug = 1;
 
-update_db();
+find(\&wanted, @filePath);
 
-sub update_db
+sub wanted
 {
+  if ($File::Find::name =~ /\.csv/)
+  {
+     my $sql = "load data local infile \"$File::Find::name\"
+               REPLACE INTO TABLE csadata FIELDS TERMINATED BY ','
+               ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES
+               (\@v_date,state,number,level,placard_inspection,hm_inspection,basic,violation_group,code,description,out_of_service,convicted_diff_charge,violation_weight,time_weight,basic_violation_inspection,unit,last_name,first_name,\@v_dob,license_state,licence_number,co_driver_last_name,co_driver_first_name,co_driver_dob,co_driver_license_state,co_driver_license,veh1_type,veh1_make,veh1_license_state,veh1_license_num,veh1_vin,veh2_type,veh2_make,veh2_license_state,veh2_license_num,veh2_vin)
+               SET id=NULL, date = str_to_date(\@v_date, '%m/%d/%Y'), dob = str_to_date(\@v_dob,'%m/%d/%Y')";
 
-	my $file = shift;
-        my $sql = "load data local infile \"$filePath\" 
-                   REPLACE INTO TABLE csadata FIELDS TERMINATED BY ',' 
-                   ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES
-                   (\@v_date,state,number,level,placard_inspection,hm_inspection,basic,violation_group,code,description,out_of_service,convicted_diff_charge,violation_weight,time_weight,basic_violation_inspection,unit,last_name,first_name,\@v_dob,license_state,licence_number,co_driver_last_name,co_driver_first_name,co_driver_dob,co_driver_license_state,co_driver_license,veh1_type,veh1_make,veh1_license_state,veh1_license_num,veh1_vin,veh2_type,veh2_make,veh2_license_state,veh2_license_num,veh2_vin)
-                   SET id=NULL, date = str_to_date(\@v_date, '%m/%d/%Y'), dob = str_to_date(\@v_dob,'%m/%d/%Y')";
+    $dbh = DBI->connect("DBI:mysql:$dbname;mysql_local_infile=1", "$dbusername", "$dbpassword") or {email_alert("Could not connect to database: $DBI::errstr")};
+    my $sth = $dbh->prepare("$sql") or
+      {email_alert("Unable to update database with file $file ". $$dbh->errstr)};
+    $sth->execute;
+    $dbh->disconnect();
+    unlink($File::Find::name);
 
-	$dbh = DBI->connect("DBI:mysql:$dbname;mysql_local_infile=1", "$dbusername", "$dbpassword") or {email_alert("Could not connect to database: $DBI::errstr")};
-	my $sth = $dbh->prepare("$sql") or
-	{email_alert("Unable to update database with file $file ". $$dbh->errstr)};
-	$sth->execute;
-	$dbh->disconnect();
-        unlink($filePath);
-        if ($sth->err())
-	{
-		return 1;
-	}else{
-		return 0;
-	}
+  }
 }
 
 sub email_alert
 {
-	my $error = shift;
+    my $error = shift;
 
-	my $toaddress = 'jaime@catalinacartage.com';
+    my $toaddress = 'jaime@catalinacartage.com';
 
-	$smtp = Net::SMTP->new('localhost');
-  
-  	$smtp->mail('ftpdaemon@catalinacartage.com');
-  	$smtp->to("$toaddress");
-  
-  	$smtp->data();
-  	$smtp->datasend("To: $toaddress\n");
-  	$smtp->datasend("Subject: Error connecting to catalina - import\n");
-  	$smtp->datasend("CC: $ccaddress\n");
-  	$smtp->datasend("\n");
-  	$smtp->datasend("$error.\n");
-  	$smtp->dataend();
-  
-  	$smtp->quit;
+    $smtp = Net::SMTP->new('localhost');
+
+    $smtp->mail('ftpdaemon@catalinacartage.com');
+    $smtp->to("$toaddress");
+
+    $smtp->data();
+    $smtp->datasend("To: $toaddress\n");
+    $smtp->datasend("Subject: Error connecting to catalina - import\n");
+    $smtp->datasend("CC: $ccaddress\n");
+    $smtp->datasend("\n");
+    $smtp->datasend("$error.\n");
+    $smtp->dataend();
+
+    $smtp->quit;
 }
 
 sub debugger
 {
-	my $message = shift;
-	if ($debug == 1)
-	{
-		print $message."\n";
-	}
+    my $message = shift;
+    if ($debug == 1)
+    {
+        print $message."\n";
+    }
 }
