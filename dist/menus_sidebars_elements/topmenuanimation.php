a091d17de74011fc7a86a18aeb96785e24de8933
jrussell55565 <jaime.russell@catalinacartage.com>
2016-01-14 00:45:05 -0700

    dont accept update
    Refuse

--- dist/menus_sidebars_elements/topmenuanimation.php
+++ dist/menus_sidebars_elements/topmenuanimation.php
@@ -8,7 +8,7 @@
                  <span class="info-box-icon bg-aqua"><i class="fa fa-cog fa-spin"></i></span>
                 </a>               
             <div class="info-box-content">
-			     <span class="info-box-text"><a href="<?php echo HTTP;?>/pages/dispatch/orders.php">Load Board</a></span><span class="info-box-number">Todays PU:<?php echo "$pu_today_count";?><br>
+			     <span class="info-box-text"><a href="/pages/dispatch/orders.php">Load Board</a></span><span class="info-box-number">Todays PU:<?php echo "$pu_today_count";?><br>
                  Todays DEL:   <?php echo "$del_today_count";?></span></div>
                  <!-- /.info-box-content -->
               </div><!-- /.info-box -->
