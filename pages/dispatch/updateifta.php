afede0a94098601055b1d788b38430a0dcca1997
mjtice <mjtice@gmail.com>
2016-09-01 18:43:26 -0600

    same options as ifta

--- pages/dispatch/updateifta.php
+++ pages/dispatch/updateifta.php
@@ -16,8 +16,8 @@
    $us_state_abbrevs = array('AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY');
   
    $compliance_options = array('Not in Packet', 'Incomplete', 'Complete', 'NA');
-   $issue_options = array('NA', 'Logs/VIR', 'No Trip Pack', 'Other');
-   $fuel_issue_options = array('NA', 'BOL', 'Fuel', 'Logs', 'VIR', 'Other');
+   $issue_options = array('NA', 'Date Missing', 'Driver Missing', 'HWB Missing', 'Route HWY Missing', 'State Ext Ent Missing', 'Miles Missing', 'Odometer Missing', 'Other');
+   $fuel_issue_options = array('NA', 'Fuel Inv # Missing', 'Fuel Gallons Missing', 'Fuel Vendor Missing', 'Fuel City Missing', 'Fuel State Missing', 'Fuel Odometer Missing', 'Other');
  
    // Get the info from the DB if this is a GET req.
    if (isset($_GET['trip_no'])) {
