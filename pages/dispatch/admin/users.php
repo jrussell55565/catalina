6eff932799f1565f79fe394bc97b1d58f6aa0021
jrussell55565 <jaime.russell@catalinacartage.com>
2016-09-29 18:14:45 -0700

    Favicon Update for Users

--- pages/dispatch/admin/users.php
+++ pages/dispatch/admin/users.php
@@ -272,6 +272,7 @@ if (! empty($_FILES["fuelUpload"]["name"]))
 <head>
 <meta charset="UTF-8">
 <title>Users</title>
+<?php require($_SERVER['DOCUMENT_ROOT'].'/dist/favicon/favicon.php');?>
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 <!-- Bootstrap 3.3.4 -->
 <link href="<?php echo HTTP;?>/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
