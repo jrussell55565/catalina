7af513d0aff7a3c8f3124397228b3d2364dc01f4
mjtice <mjtice@gmail.com>
2017-02-05 12:45:10 -0700

    better error capturing

--- pages/dispatch/pdf.php
+++ pages/dispatch/pdf.php
@@ -1,38 +1,56 @@
 <?php
 session_start();
 
-$target_file = '/tmp/'.$_SESSION['username'].'_upload.pdf';;
-copy("php://input", $target_file);
+include("$_SERVER[DOCUMENT_ROOT]/dist/php/global.php");
+$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_name);
+$message_output = null;
+$header = null;
 
-// Open the file and read it.
-$handle = fopen($target_file, "r");
-if ($handle) {
-    $counter = 0;
-    while (($line = fgets($handle)) !== false) {
-        if (preg_match('/^.+filenameis_(\w+\.pdf).+$/', $line, $matches, PREG_OFFSET_CAPTURE))
-        {
-            $counter++;
-            $file_name = $_SERVER['DOCUMENT_ROOT'] . '/pages/onboarding/user_uploads/' . $matches[1][0];
-            if (rename($target_file, $file_name))
+try {
+    $target_file = '/tmp/'.$_SESSION['username'].'_upload.pdf';;
+    if (copy("php://input", $target_file))
+    {
+        // Open the file and read it.
+        $handle = fopen($target_file, "r");
+        if ($handle) {
+            $counter = 0;
+            while (($line = fgets($handle)) !== false) {
+                if (preg_match('/^.+filenameis_(\w+\.pdf).+$/', $line, $matches, PREG_OFFSET_CAPTURE))
+                {
+                    $counter++;
+                    $file_name = $_SERVER['DOCUMENT_ROOT'] . '/pages/onboarding/user_uploads/' . $_SESSION['username'].'_'.$matches[1][0];
+                    if (rename($target_file, $file_name))
+                    {
+                        $message_output = 'File upload was successful';
+                        $header = 200;
+                    }else{
+                        throw new Exception('Unable to move '.$target_file.' to '.$file_name, 1);
+                    }
+                }
+            }
+            if ($counter == 0)
             {
-                fclose($handle);
-                header('Content-Type: text/plain; charset=utf-8');
-                return 'File upload was successful';
-            }else{
-                fclose($handle);
-                header('Content-Type: text/plain; charset=utf-8');
-                error_log('Unable to move '.$target_file.' to '.$file_name);
-                return 'File upload failed';
+                throw new Exception('Unable to determine the pdf filename in '.$target_file, 1);
             }
         }
+    } else {
+        throw new Exception("Unable to rename temporary file.", 1);
     }
+}catch (Exception $e) {
+    $message_output = $e;
+    error_log($e);
+    $header = 500;
+}finally{
     fclose($handle);
-    if ($counter == 0)
-    {
-        header('Content-Type: text/plain; charset=utf-8');
-        error_log('Unable to find filename in '.$target_file);
-        fclose($handle);
-        return 'File upload failed';
+    header('Content-Type: text/plain; charset=utf-8');
+    if ($header == 500) {
+        http_response_code(500);
+        //header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    }else{
+        http_response_code(200);
+        //header($_SERVER['SERVER_PROTOCOL'] . ' OK', true, 200);
     }
+    echo $message_output;
+    exit;
 }
 ?>
